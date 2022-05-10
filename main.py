472296b3f932beb90f0ce944f0f8c870d4ae85c6
Frank Hou <93571148+pseudocode-master64@users.noreply.github.com>
2022-05-10 12:46:03 +0800

    Update main.py
    - Updated UI
    - Added a new function -count- to the original program
    - Added time() to simulated real-life calculation
    - Optimized code base, cutting unnecessary lines

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
+
 class Tower: 
-    
+
     def __init__(self) -> str:
         total_rings = int(input('How many rings are there? '))
         self.total_rings: int = total_rings
@@ -16,34 +18,40 @@ class Tower:
             (self.original).insert(0,i+1)
             
         self.display()
+
+        self.count = 0
             
     def display(self) -> None:
-        print()
-        print('A ', self.pole['A'])
-        print('B ', self.pole['B'])
-        print('C ', self.pole['C'])
-        print()
+        """display the current version of tower of hanoi"""
+
+        print("\n"'=============')
+        for i in self.pole:
+            print(i, self.pole[i])
+        print('============='"\n")          
 
     def move_ring(self, move_from, move_to) -> str:
             """Move one ring from one position to another"""
 
-            if len(self.pole[move_from]) == 0:
-                ring = self.pole[move_from][0]
-            else:
-                ring = self.pole[move_from][-1]
+            if not self.pole[move_from]:
+                print("\n"'##### There is no ring at the source pole, try again #####')
+                return
+
+            ring = self.pole[move_from][-1]
 
             # Exceptions
             if len(self.pole[move_to])!= 0 and ring > self.pole[move_to][-1]:
-                print('The ring you asked to move is BIGGER than the last ring on the destination pole, try again')
-            else:    
-                self.pole[move_from].pop()
-                self.pole[move_to].append(ring)
+                print("\n"'##### The ring you want to move is BIGGER than the ring on the desination pole, try again #####')
+
+            self.pole[move_from].pop()
+            self.pole[move_to].append(ring)
+            self.count += 1
 
     def autoplay(self, biggest_ring: int, source: str, destination: str, auxilary: str) -> None:
         """Automatically solve the puzzle + print out each step taken to solve the puzzle"""
-        
+
         if biggest_ring == self.pole[source][-1]: # if the biggest ring is the last ring on the source pole
             self.move_ring(source, destination)
+            print(f'Move #{self.count}')
             self.display()
         else:
             index = self.pole[source].index(biggest_ring) # .index(a) returns the index of a within a list
@@ -51,26 +59,30 @@ class Tower:
             
             self.autoplay(top_ring, source, auxilary, destination) # destination is changed to auxilary pole
             self.move_ring(source, destination)
+            print(f'Move #{self.count}')
             self.display()
             self.autoplay(top_ring, auxilary, destination, source) # auxilary pole is changed to source pole
-
 
 
     def execute(self) -> str:
         """Initiate the game"""
 
-        if input("Do you want to help? (Y/N) ") == "Y":
+        if input("Do you want to help? (Yes/No) ") == "Yes":
+                time.sleep(1)
                 self.autoplay(self.total_rings,'A', 'C', 'B')
+                print(f"--- The optimized move count is {self.count} ---")
         else:
+            print("\n")
             pass
 
         while True:
             if self.original == self.pole['C']:
-                print("\n"'Congratulations! You solved it!'"\n")
+                print("\n"'--- Congratulations! You solved it! ---'"\n")
                 break 
             else:
                 Start.move_ring(str(input('From which pole? ')), str(input('To which pole? ')))
                 self.display()
+                print(f'--- {self.count} move(s) have been made ---'"\n")
            
 Start = Tower()
 Start.execute()
